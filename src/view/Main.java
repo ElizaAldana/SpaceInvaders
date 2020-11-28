ab075799a1bfa58678ee45946313a443a2b8e324
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-28 07:59:19 -0500

    clases y atributos de la nave

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,24 @@
 package view;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main("view.Main");
+	}
+	
+	public void settings() {
+		size(600,600);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		background(0);
 	}
 
 }
